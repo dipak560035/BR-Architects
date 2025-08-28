7e563843bd210f5202b70f1688790373255e6465
dipak560035 <dipaksah2070@gmail.com>
2025-08-28 20:03:15 +0545

    Fix HouseData export/import and ProjectImage overlay styling

--- src/home/Project.jsx
+++ src/home/Project.jsx
@@ -1,39 +1,64 @@
-// import { houseData } from "../data/HouseData";
-// import Projectimg from "./Projectimg";
+// // import { houseData } from "../data/HouseData";
+// // import Projectimg from "./Projectimg";
+
+
+// // export default function Project() {
+// //   return (
+// //     <div className="px-7 my-15">
+
+
+// //  <h1>Project</h1>
+// //  <hr className="my-4 text-gray-400" />
+
+
+// // <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 p-5">
+// // {houseData.map((house) => {
+// //   return <Projectimg
+// //   key={house.id}
+// //   image={house.image}
+// //   label={house.name} />
+
+// // })}
+
+
+// // </div>
+
+
+
+
+// //  </div>
+// //   )
+// // }
+
+
+// import  houseData  from "../data/HouseData";
+// import ProjectImage from "./ProjectImage";
 
 
 // export default function Project() {
 //   return (
-//     <div className="px-7 my-15">
+//     <section id="projects" className="scroll-mt-24 px-7 my-15">
+//       <h1 className="text-2xl font-bold text-gray-900">Project</h1>
+//       <hr className="my-4 text-gray-400" />
 
-
-//  <h1>Project</h1>
-//  <hr className="my-4 text-gray-400" />
-
-
-// <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 p-5">
-// {houseData.map((house) => {
-//   return <Projectimg
-//   key={house.id}
-//   image={house.image}
-//   label={house.name} />
-
-// })}
-
-
-// </div>
-
-
-
-
-//  </div>
-//   )
+//       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 p-5">
+//         {houseData.map((house) => (
+//           <ProjectImage
+//             key={house.id}
+//             image={house.image}
+//             label={house.name}
+//           />
+//         ))}
+//       </div>
+//     </section>
+//   );
 // }
+
+
 
 
 import { houseData } from "../data/HouseData";
 import ProjectImage from "./ProjectImage";
-import Projectimg from "./ProjectImage";
 
 export default function Project() {
   return (
@@ -53,5 +78,4 @@ export default function Project() {
     </section>
   );
 }
-
 
--- src/home/ProjectImage.jsx
+++ src/home/ProjectImage.jsx
@@ -28,12 +28,29 @@
 //   );
 // }
 
+// export default function ProjectImage({ image, label }) {
+//   return (
+//     <div className="relative">
+//       <img src={image} alt="" />
+//       <h1 className="absolute top-0 text-white text-lg bg-black p-2">{label}</h1>
+//     </div>
+//   )
+// }
+
 export default function ProjectImage({ image, label }) {
   return (
-    <div className="relative">
-      <img src={image} alt="" />
-      <h1 className="absolute top-0 text-white text-lg bg-black p-2">{label}</h1>
-    </div>
-  )
-}
+    <div className="relative w-full h-60 overflow-hidden rounded-lg shadow-md">
+      {/* Image */}
+      <img
+        src={image}
+        alt={label}
+        className="w-full h-full object-cover"
+      />
 
+      {/* Label overlay (top-left corner) */}
+      <h1 className="absolute top-0 left-0 bg-black bg-opacity-70 text-white text-sm px-3 py-1 rounded-br-lg">
+        {label}
+      </h1>
+    </div>
+  );
+}
