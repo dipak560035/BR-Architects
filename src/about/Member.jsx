3010ca8135b26463ab0cad48393b0b7a8d8c7fb6
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-04-27 21:06:49 +0545

    Update Member.jsx

--- src/about/Member.jsx
+++ src/about/Member.jsx
@@ -2,7 +2,7 @@
 
 // export default function Member() {
 //   return (
-//     <div>
+//     <div
 //         <div>
 //             <img src="https://www.w3schools.com/w3images/team2.jpg" alt="" />
 //         </div>
