6664b6751417e4dc9516a9f6f8b581b3a4d469b3
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-08-20 20:11:34 +0545

    Update AboutSection.jsx

--- src/about/AboutSection.jsx
+++ src/about/AboutSection.jsx
@@ -15,11 +15,7 @@ export default function AboutSection() {
        
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 p-5">
-        {memberData.map((member) => (
-          <Member key={member.id} member={member} />
-        ))}
-      </div>
+  
     </section>
   );
 }
